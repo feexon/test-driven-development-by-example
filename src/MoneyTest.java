666474fedb22b5c7ce3513bc1f37f0b7d360acef
yaho <feexon@qq.com>
2014-03-09 01:07:42 +0800

    add a failing test

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -2,5 +2,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MoneyTest{
+	
+	@Test
+	public void testMultiplication(){
+		Dollar five=new Dollar(5);
+		five.times(2);
+		assertEquals(10,five.amount);
+	}
 
 }
