8b5745b91eb85aa4556123fe4f4db80c0bdd020a
yaho <feexon@qq.com>
2014-03-09 15:46:18 +0800

    add a failing test for equality

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -11,5 +11,10 @@ public class MoneyTest{
 		product=five.times(3);
 		assertEquals(15,product.amount);
 	}
+	
+	@Test
+	public void testEquality(){
+		assertEquals(new Dollar(5),new Dollar(5));
+	}
 
 }
