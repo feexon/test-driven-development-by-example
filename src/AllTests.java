342ccf56d1f2c48a7b5ecb8bffce4ed8a93fe085
yaho <feexon@qq.com>
2014-03-18 23:23:55 +0800

    add a suite test for AllTests

---
+++ src/AllTests.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@SuiteClasses({MoneyTest.class})
+@RunWith(Suite.class)
+public class AllTests{
+}
